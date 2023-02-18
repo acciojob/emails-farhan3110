fc86943fb8e6c759b972b87d63ea630f742d48f8
farhan3110 <mdfarhan3110@gmail.com>
2023-02-18 22:59:59 +0530

    Second attempt

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -25,15 +25,15 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        Collections.sort(calendar, (m1, m2) -> m1.getEnd().compareTo(m2.getEnd()));
+        Collections.sort(calendar, (m1, m2) -> m1.getEndTime().compareTo(m2.getEndTime()));
 
         // Find the maximum number of meetings you can attend
         int maxMeetings = 0;
         LocalTime previousEnd = LocalTime.MIN;
         for (Meeting meeting : calendar) {
-            if (meeting.getStart().compareTo(previousEnd) >= 0) {
+            if (meeting.getStartTime().compareTo(previousEnd) >= 0) {
                 maxMeetings++;
-                previousEnd = meeting.getEnd();
+                previousEnd = meeting.getEndTime();
             }
         }
         return maxMeetings;
